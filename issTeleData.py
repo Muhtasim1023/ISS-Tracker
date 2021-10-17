2cbb0610710f56e4ff38966e617d4a56786d1e92
Muhtasim1023 <41923336+Muhtasim1023@users.noreply.github.com>
2021-10-16 23:19:08 -0400

    Update issTeleData.py

--- issTeleData.py
+++ issTeleData.py
@@ -1,13 +1,40 @@
 import requests as rq
+import serial
 import time
 
+arduino = serial.Serial(port='COM3', baudrate=9600, timeout=.1)
 
-while(True):
-    response = rq.get("http://api.open-notify.org/iss-now.json").json()
-    longitude = float(response["iss_position"]["longitude"])
-    latitude = float(response["iss_position"]["latitude"])
-    print(longitude,",",latitude)
-    time.sleep(1)
+def write_read(x):
+    arduino.write(bytes(x, 'utf-8'))
+    time.sleep(0.05)
+    data = arduino.readline()
+    return data
+
+while True:
+    num = input("Enter a number: ") # Taking input from user
+    value = write_read(num)
+    print(value) # printing the value
 
 
+
+#arduino = serial.Serial(port='COM3', baudrate=9600, timeout=.1)
+#
+#def write_read(x):
+#    arduino.write(bytes(x, 'utf-8'))
+#    time.sleep(0.05)
+#    data = arduino.readline()
+#    return data
+#
+#position = []
+#
+#while True:
+#    response = rq.get("http://api.open-notify.org/iss-now.json").json()
+#    longitude = float(response["iss_position"]["longitude"])
+#    latitude = float(response["iss_position"]["latitude"])
+#    position = [longitude, latitude]
+#    value = write_read(position)
+#    print(value)
+#    time.sleep(1)
+#
+#
 
